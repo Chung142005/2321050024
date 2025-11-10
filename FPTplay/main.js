2f4356c0e23d977d8bc3a040befdb5fe7ed1572e
Chung <cn268471@gmail.com>
2025-11-10 11:10:36 +0700

    code ngày 10/11/2025

--- FPTplay/main.js
+++ FPTplay/main.js
@@ -19,6 +19,36 @@ let danhSachPhim = [
     poster: "img/one-punch-man.png",
     theLoai: "Phim hoạt hình",
   },
+  {
+    id: 3,
+    tenPhim: "Tình Cuồng Si",
+    namPhatHanh: 2008,
+    tuoi: "T13",
+    thoiLuong: "2 giờ",
+    quocGia: "Thái Lan",
+    poster: "img/tinh_cuong_si_poster.png",
+    theLoai: "Phim tình cảm",
+  },
+  {
+    id: 4,
+    tenPhim: "Thời Tới Cản Không Kịp",
+    namPhatHanh: 2022,
+    tuoi: "T13",
+    thoiLuong: "2 giờ",
+    quocGia: "Việt Nam",
+    poster: "./img/thoi_toi_can_khong_kip_poster.png",
+    theLoai: "Phim chiếu rạp",
+  },
+  {
+    id: 5,
+    tenPhim: "Độc Thân Tuổi 30",
+    namPhatHanh: 2014,
+    tuoi: "T13",
+    thoiLuong: "2 giờ",
+    quocGia: "Việt Nam",
+    poster: "./img/doc_than_tuoi_30_poster.png",
+    theLoai: "Phim chiếu rạp",
+  },
 ];
 let phimHienTai = danhSachPhim[0];
 
