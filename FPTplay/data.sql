35b478c8047e6bffc63fc7eaddf7855e65a0498f
Chung <cn268471@gmail.com>
2025-11-14 11:43:02 +0700

    code ngày 14/11/2025

---
+++ FPTplay/data.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE IF NOT EXISTS quan_ly_web_xem_phim
+USE quan_ly_web_xem_phim
+
+
+1. quản lý người dùng
+    - id INT
+    -ten dang nhap
+    -mat khau
+    -email
+    -tên người dùng nvarchar
+    -giới tính 
+    -ngay sinh datetime
+    -sdt char
+    -
+2. thể loại phim
+    -mã thể loại INT
+    -tên thể loại nvarchar
+3. thông tin phim
+    -tên thể loại nvarchar
+	-số lượng tập phim INT
+    -năm phát hành INT
+	-tuổi xem INT
+	-poster varchar
+    -quoc gia id INT
+	-tên diễn viên 
+    -mô tả text
+	
+4. lịch sử xem
+5. diễn viên
+    -id INT
+    -phim id INT
+    -dien vien id INT
+	-tên diễn viên nvarchar
+	-tuổi diễn viên INT
+	-phim nổi bật 
+6. quốc gia
+    -id
+    -
+7. tập phim
+    -
+
